8e3624bc8982c17e277d877fab650711f49cc121
lzsjqq <lzsjqq@aliyun.com>
2020-01-01 23:25:33 +0800

    概念

--- wheat/wheat-biz/src/main/java/com/farm/wheat/share/biz/constant/ShareConst.java
+++ wheat/wheat-biz/src/main/java/com/farm/wheat/share/biz/constant/ShareConst.java
@@ -10,4 +10,5 @@ public class ShareConst {
     public  static  final  String SHARE_INFO_BY_TB_FIELD="shareInfoDTOList";
     public  static  final  String SOURCE_TX="TX";
     public  static  final  String SOURCE_WY="WY";
+    public static final String CONCEPT_DETAIL_LIST = "concept_detail_list";
 }
