3ef35f20c6e4e3ee94a7c11a5e6da273ac2df6f8
lzsjqq <lzsjqq@aliyun.com>
2020-03-13 21:56:39 +0800

    构造顶分型

---
+++ wheat/wheat-chan/src/main/java/com/farm/wheat/share/chan/util/LinkedUtil.java
@@ -0,0 +1,24 @@
+package com.farm.wheat.share.chan.util;
+
+import java.util.List;
+
+/**
+ * @program: wheat
+ * @description: 用于构造处理链表
+ * @author: xyc
+ * @create: 2020-03-13 21:49
+ */
+public class LinkedUtil {
+    private LinkedUtil() {
+    }
+
+    /**
+     * 构建链表
+     *
+     * @return
+     */
+    public static List<PriceNode> buildLined() {
+
+        return null;
+    }
+}
